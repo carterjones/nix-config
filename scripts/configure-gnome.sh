576cde375a4449ceac17f9485dad1aebe022112f
Carter Jones <cjones@cigital.com>
2016-08-06 10:03:14 -0700

    add more gnome customizations

--- scripts/configure-gnome.sh
+++ scripts/configure-gnome.sh
@@ -6,3 +6,7 @@ set -eux -o pipefail
 PROFILE_PATH=/org/gnome/terminal/legacy/profiles:/
 PROFILE=$(dconf list $PROFILE_PATH)
 dconf write ${PROFILE_PATH}${PROFILE}scrollback-unlimited true
+
+# Set up workspaces
+gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ hsize 3
+gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ vsize 3
---
+++ scripts/install-compizconfig-settings-manager.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -eux -o pipefail
+
+sudo apt-get install -y \
+    compizconfig-settings-manager
+
