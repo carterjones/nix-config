f9db7e9b71f84d07e61b56eeb8da577c1f48e717
Carter Jones <cjones@cigital.com>
2016-08-06 09:44:57 -0700

    alphabetize dot files

--- scripts/create-dot-files.sh
+++ scripts/create-dot-files.sh
@@ -3,6 +3,6 @@
 set -eux -o pipefail
 
 # Set up bashrc related files.
-cp ./files/.bashrc ~/.bashrc
 cp ./files/.bash_aliases ~/.bash_aliases
+cp ./files/.bashrc ~/.bashrc
 cp ./files/.exports ~/.exports
