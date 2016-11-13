082c7e5a41c77e7e940e61e97d6fc8c4983e9c1d
Carter Jones <carter@carterjones.info>
2016-11-13 13:00:21 -0800

    share gitconfig cross platform

--- scripts/shared/create-dot-files.sh
+++ scripts/shared/create-dot-files.sh
@@ -9,11 +9,11 @@ cp ${shared_files}/.bash_prompt $HOME/
 cp ${shared_files}/.bashrc $HOME/
 cp ${shared_files}/.exports $HOME/
 cp ${shared_files}/.emacs $HOME/
+cp ${shared_files}/.gitconfig $HOME/
 cp ${shared_files}/.git-completion $HOME/
 cp ${shared_files}/.tmux.conf $HOME/
 
 if [[ $(uname) == Linux ]]; then
-    cp ${linux_files}/.gitconfig $HOME/
     cp ${linux_files}/.nanorc $HOME/
 elif [[ $(uname) == Darwin ]]; then
     cp ${macos_files}/.bash_profile $HOME/
