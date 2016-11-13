4917bdcb0f4fad9803eae9bfc998f54e7f70644d
Carter Jones <carter@carterjones.info>
2016-11-13 12:18:50 -0800

    begin migrating the dot files

--- scripts/linux/create-dot-files.sh
+++ scripts/linux/create-dot-files.sh
@@ -2,13 +2,19 @@
 
 set -eux -o pipefail
 
-# Set up bashrc related files.
-cp ${linux_files}/.bash_aliases $HOME/
-cp ${linux_files}/.bash_prompt $HOME/
-cp ${linux_files}/.bashrc $HOME/
-cp ${linux_files}/.exports $HOME/
-cp ${linux_files}/.emacs $HOME/
-cp ${linux_files}/.git-completion $HOME/
-cp ${linux_files}/.gitconfig $HOME/
-cp ${linux_files}/.nanorc $HOME/
-cp ${linux_files}/.tmux.conf $HOME/
+# Set up Bash related files.
+cp ${shared_files}/.bash_aliases $HOME/
+cp ${shared_files}/.bash_completion $HOME/
+cp ${shared_files}/.bash_prompt $HOME/
+cp ${shared_files}/.bashrc $HOME/
+cp ${shared_files}/.exports $HOME/
+cp ${shared_files}/.emacs $HOME/
+cp ${shared_files}/.git-completion $HOME/
+cp ${shared_files}/.nanorc $HOME/
+cp ${shared_files}/.tmux.conf $HOME/
+
+if [[ $(uname) == Linux ]]; then
+    cp ${linux_files}/.gitconfig $HOME/
+elif [[ $(uname) == Darwin ]]; then
+    cp ${macos_files}/.bash_profile $HOME/
+fi
