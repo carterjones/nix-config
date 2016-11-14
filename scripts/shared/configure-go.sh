b4a2fb38a8db650feb0dbbeecc8be89c54f48314
Carter Jones <carter@carterjones.info>
2016-11-13 21:42:49 -0800

    more emacs golang dependencies

--- scripts/shared/configure-go.sh
+++ scripts/shared/configure-go.sh
@@ -7,6 +7,7 @@ go get -u github.com/kardianos/govendor
 
 # Install stuff for emacs golang integration.
 go get -u github.com/golang/lint/golint
+go get -u github.com/nsf/gocode
 go get -u golang.org/x/tools/cmd/goimports
 
 popd
