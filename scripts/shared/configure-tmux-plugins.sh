4e64957e8e65e31d33f951217b1371ca11c352fe
Carter Jones <carter@carterjones.info>
2016-11-13 16:35:54 -0800

    add tmux resurrect plugin

---
+++ scripts/shared/configure-tmux-plugins.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Set up the tmux plugins.
+mkdir -p $CODE/github.com/tmux-plugins
+pushd $CODE/github.com/tmux-plugins
+
+# Clone the tmux-resurrect repo if it does not exist.
+[ ! -d tmux-resurrect ] && git clone https://github.com/tmux-plugins/tmux-resurrect
+
+# Update the tmux-resurrect repo.
+cd $CODE/github.com/tmux-plugins/tmux-resurrect
+git pull
