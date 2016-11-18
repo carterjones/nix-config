467759b13a12d378a73fc2c8df0d7daa2075d551
Carter Jones <carter@carterjones.info>
2016-11-18 15:19:24 -0800

    add support for yaml mode

--- scripts/shared/configure-emacs.sh
+++ scripts/shared/configure-emacs.sh
@@ -5,3 +5,6 @@ mkdir -p $dependency_path
 
 fci_url="https://raw.githubusercontent.com/alpaker/Fill-Column-Indicator/master/fill-column-indicator.el"
 curl $fci_url > $dependency_path/fill-column-indicator.el
+
+yaml_url="https://raw.githubusercontent.com/yoshiki/yaml-mode/master/yaml-mode.el"
+curl $fci_url > $dependency_path/yaml-mode.el
