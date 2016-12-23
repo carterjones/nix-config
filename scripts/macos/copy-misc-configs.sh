4e7464baf2c92a3fdd6debaabbb6be8da6ca01d5
Carter Jones <carter@carterjones.info>
2016-12-23 01:13:55 -0800

    back up cool retro term config

---
+++ scripts/macos/copy-misc-configs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Copy the cool-retro-term config file to the home directory. This
+# doesn't configure it, but it does make it available in the home
+# directory.
+# More info here: https://github.com/Swordfish90/cool-retro-term
+cp $macos_files/.cool-retro-term.json $HOME/
