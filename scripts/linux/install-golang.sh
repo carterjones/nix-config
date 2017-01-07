3dc630c13df0d9a3419a0395dba135b53c3f64b7
Carter Jones <carter@carterjones.info>
2017-01-07 09:59:58 -0800

    iterate on golang installer

--- scripts/linux/install-golang.sh
+++ scripts/linux/install-golang.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+set -eux -o pipefail
 
 archive_name="go1.7.4.linux-amd64.tar.gz"
 golang_url="https://storage.googleapis.com/golang/${archive_name}"
 
 pushd /tmp
 
-wget $golang_url
+wget -q $golang_url
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf $archive_name
 sudo update-alternatives --install "/usr/bin/go" "go" "/usr/local/go/bin/go" 0
