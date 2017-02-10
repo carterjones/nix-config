10f10fe6105c485857cfdc2b215d3a8a1ce2ae20
Carter Jones <carter@carterjones.info>
2017-02-09 17:41:54 -0800

    add newline-and-indent

--- files/shared/.emacs.d/site-init/keyboard-shortcuts.el
+++ files/shared/.emacs.d/site-init/keyboard-shortcuts.el
@@ -1,2 +1,5 @@
 ;; Keyboard shortcuts.
 (global-set-key (kbd "M-e") 'project-explorer-toggle)
+
+;; Reindent, followed by newline and indent.
+(global-set-key (kbd "RET") 'reindent-then-newline-and-indent)
