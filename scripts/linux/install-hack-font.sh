1d944fd4ea724c3facca66e02bc5406cc91b02cd
Carter Jones <carter@carterjones.info>
2017-02-11 16:05:17 -0800

    install hack font

---
+++ scripts/linux/install-hack-font.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget -nc "https://github.com/chrissimpkins/Hack/releases/download/v2.020/Hack-v2_020-ttf.zip"
+unzip -u Hack-v2_020-ttf.zip
+sudo cp -f Hack-*.ttf /usr/share/fonts/
+fc-cache -f -v
