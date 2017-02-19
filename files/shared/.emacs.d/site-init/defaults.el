74a7bac3e1096a5b2fb3f11a5ec96112d0236d52
Carter Jones <carter@carterjones.info>
2017-02-19 02:13:17 -0800

    fix open in chrome for mac

--- files/shared/.emacs.d/site-init/defaults.el
+++ files/shared/.emacs.d/site-init/defaults.el
@@ -14,7 +14,10 @@
 (menu-bar-mode -1)
 
 ;; Set the default browser to Chrome.
-(setq browse-url-browser-function 'browse-url-chrome)
+(if (eq system-type 'darwin)
+    (setq browse-url-generic-program "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+          browse-url-browser-function 'browse-url-generic)
+  (setq browse-url-browser-function 'browse-url-chrome))
 
 ;; Load changes from disk into buffer if changes are made outside of Emacs.
 (global-auto-revert-mode t)
