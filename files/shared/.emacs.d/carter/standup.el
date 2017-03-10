677a688190cf81d97513f014069a49b75d13041a
Carter Jones <carter@carterjones.info>
2017-03-10 11:44:15 -0800

    massive refactor

--- files/shared/.emacs.d/carter/standup.el
+++ files/shared/.emacs.d/carter/standup.el
@@ -1,5 +1,5 @@
-(defun add-todo-section ()
-  "Create a new TODO entry."
+(defun standup-todo-new-section ()
+  "Create a new TODO section."
   (interactive)
   (let ((beg (progn (search-backward "###")
                     (search-forward "blocker")
@@ -19,7 +19,34 @@
     (next-line)
     (move-end-of-line nil)))
 
-(defun import-pivotal-tasks ()
+(defun standup-todo-mark-as-done ()
+  (interactive)
+  (progn
+    (move-beginning-of-line nil)
+    (kill-visual-line 1)
+    (search-backward "Done")
+    (search-forward "\n\n")
+    (previous-line)
+    (yank)
+    (previous-line)))
+
+(defun standup-todo-fold-except-top ()
+  (interactive)
+  ;; Move to top header and collapse everything.
+  (beginning-of-buffer)
+  (markdown-cycle)
+  ;; Unfold the next level of headers and move down.
+  (markdown-cycle)
+  (next-line 2)
+  ;; Unfold the next level of headers and move down.
+  (markdown-cycle)
+  (next-line 2)
+  ;; Unfold the last level of headers.
+  (markdown-cycle)
+  ;; Move to the end of the TODO line within this section.
+  (search-forward "TODO:"))
+
+(defun standup-pivotal-import-tasks ()
   (interactive)
   (save-excursion
     (progn
@@ -29,9 +56,13 @@
                (concat
                 "/bin/bash --login $HOME/bin/pi | "
                 "/usr/local/bin/jq -r '.stories.stories[].name' | "
-                "sed 's,^,- [sec] ,'"))))))
+                "sed 's,^,- [sec] ,'")))
+      (set-mark-command nil)
+      (search-backward "TODO")
+      (delete-duplicate-lines (region-beginning) (region-end))
+      (deactivate-mark))))
 
-(defun create-or-update-pivotal-task (action)
+(defun standup-pivotal-create-or-update-task (action)
   (interactive)
   (save-excursion
     (progn
@@ -48,46 +79,24 @@
         "> /dev/null"))
       (deactivate-mark))))
 
-(defun add-task-to-pivotal ()
+(defun standup-pivotal-new-midweek-task ()
   (interactive)
   (save-excursion
-    (create-or-update-pivotal-task "new-midweek-done")))
+    (standup-pivotal-create-or-update-task "new-midweek")))
 
-(defun mark-as-done ()
+(defun standup-pivotal-deliver-task ()
   (interactive)
-  (progn
-    (move-beginning-of-line nil)
-    (kill-visual-line 1)
-    (search-backward "Done")
-    (search-forward "\n\n")
-    (previous-line)
-    (yank)
-    (previous-line)
-    (create-or-update-pivotal-task "done")))
+  (save-excursion
+    (standup-pivotal-create-or-update-task "done")))
 
-(defun fold-todos-except-top ()
-  (interactive)
-  ;; Move to top header and collapse everything.
-  (beginning-of-buffer)
-  (markdown-cycle)
-  ;; Unfold the next level of headers and move down.
-  (markdown-cycle)
-  (next-line 2)
-  ;; Unfold the next level of headers and move down.
-  (markdown-cycle)
-  (next-line 2)
-  ;; Unfold the last level of headers.
-  (markdown-cycle)
-  ;; Move to the end of the TODO line within this section.
-  (search-forward "TODO:"))
+;; Set up shortcuts that affect just this file.
+(global-set-key (kbd "C-c s t n") 'standup-todo-new-section)
+(global-set-key (kbd "C-c s t d") 'standup-todo-mark-as-done)
+(global-set-key (kbd "C-c s t f") 'standup-todo-fold-except-top)
 
-
-
-;; Set up shortcuts.
-(global-set-key (kbd "C-c t t") 'add-todo-section)
-(global-set-key (kbd "C-c t a") 'add-task-to-pivotal)
-(global-set-key (kbd "C-c t i") 'import-pivotal-tasks)
-(global-set-key (kbd "C-c t d") 'mark-as-done)
-(global-set-key (kbd "C-c t f") 'fold-todos-except-top)
+;; Set up shortcuts that interact with Pivotal.
+(global-set-key (kbd "C-c s p i") 'standup-pivotal-import-tasks)
+(global-set-key (kbd "C-c s p n") 'standup-pivotal-new-midweek-task)
+(global-set-key (kbd "C-c s p d") 'standup-pivotal-deliver-task)
 
 (provide 'standup)
