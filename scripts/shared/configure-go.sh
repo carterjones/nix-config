de676fcc7ad11b8f5d641cd5661af5652295d2b3
Carter Jones <carter@carterjones.info>
2017-03-16 00:17:39 -0700

    allow user to skip go configuration

--- scripts/shared/configure-go.sh
+++ scripts/shared/configure-go.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-
 set -eux -o pipefail
 
-pushd $GOPATH
+# Check for a flag that can be set to avoid configuring go imports.
+if [[ "$SKIP_GO_CONFIGURATION" != "true" ]]; then
+    pushd $GOPATH
 
-# Install govendor.
-go get -u github.com/kardianos/govendor
+    # Install govendor.
+    go get -u github.com/kardianos/govendor
 
-# Install stuff for emacs golang integration.
-go get -u github.com/golang/lint/golint
-go get -u github.com/nsf/gocode
-go get -u golang.org/x/tools/cmd/goimports
+    # Install stuff for emacs golang integration.
+    go get -u github.com/golang/lint/golint
+    go get -u github.com/nsf/gocode
+    go get -u golang.org/x/tools/cmd/goimports
 
-popd
+    popd
+fi
