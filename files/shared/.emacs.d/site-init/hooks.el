cbd2e7b56253afc54d3d14c19d18113e1af0d590
Carter Jones <carter@carterjones.info>
2017-03-17 04:48:34 +0000

    treat hcl files as json

--- files/shared/.emacs.d/site-init/hooks.el
+++ files/shared/.emacs.d/site-init/hooks.el
@@ -9,3 +9,6 @@
 ;; Activate org-indent-mode.
 ;; Based on http://stackoverflow.com/a/1775652
 (add-hook 'org-mode-hook (lambda () (org-indent-mode t)) t)
+
+;; Treate HCL files as JSON.
+(add-to-list 'auto-mode-alist '("\\.hcl\\'" . json-mode))
