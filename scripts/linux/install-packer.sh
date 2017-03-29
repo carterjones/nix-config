e38d71f45579399b63143ab9eeaab79b9f1c89da
Carter Jones <carter@carterjones.info>
2017-03-29 10:01:31 -0700

    add packer install script

---
+++ scripts/linux/install-packer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -eux -o pipefail
+
+pushd /tmp
+
+wget -nc "https://releases.hashicorp.com/packer/0.12.3/packer_0.12.3_linux_amd64.zip" -O packer.zip
+unzip packer.zip
+mv packer $HOME/bin/
+
+popd
