bef18123453ef317e1733c86072dba208b91856c
Carter Jones <carter@carterjones.info>
2017-06-16 18:19:33 +0000

    one python configuration script to rule them all

--- scripts/linux/install-python.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eux -o pipefail
-
-pushd /tmp
-
-pip install --upgrade pip
-sudo -H pip install virtualenv virtualenvwrapper
-
-popd
--- scripts/shared/configure-python.sh
+++ scripts/shared/configure-python.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+set -eux -o pipefail
 
-pip3 install virtualenv
+latest_3_version=$(pyenv install --list | \
+                       grep " 3." | \
+                       grep -v dev | \
+                       tail -1 | \
+                       sed "s/.*3/3/")
+pyenv install -s $latest_3_version
+pyenv global $latest_3_version
