db59fc34aa46dfc5d8cf9104409d4a4d9ca3e801
Carter Jones <carter@carterjones.info>
2017-06-16 11:28:57 -0700

    install pyenv using a convenient installer

--- scripts/shared/configure-python.sh
+++ scripts/shared/configure-python.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -eux -o pipefail
 
+# Note: this is *not* getting run as root.
+if [[ $EUID -eq 0 ]]; then
+    echo "Do not run this script as root."
+    exit 1
+fi
+
+curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+
 latest_3_version=$(pyenv install --list | \
                        grep " 3." | \
                        grep -v dev | \
