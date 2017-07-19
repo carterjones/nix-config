77e9472c904d028eed875dfcaa16628d5f248565
Carter Jones <carter@carterjones.info>
2017-07-19 02:40:33 -0700

    add godef for emacs

--- scripts/shared/configure-go.sh
+++ scripts/shared/configure-go.sh
@@ -15,7 +15,7 @@ if [[ "$SKIP_GO_CONFIGURATION" != "true" ]]; then
     go get -u github.com/golang/lint/golint
     go get -u github.com/nsf/gocode
     go get -u golang.org/x/tools/cmd/goimports
-
+    go get -u github.com/rogpeppe/godef
 
 fi
 
