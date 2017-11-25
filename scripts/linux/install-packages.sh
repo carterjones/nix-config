683abb363337ee1d22ac6bcad44a98fd37bd520c
Carter Jones <carter@carterjones.info>
2017-11-24 23:08:38 -0800

    manjaro

--- scripts/linux/install-packages.sh
+++ scripts/linux/install-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-if cat /etc/issue | grep "Arch" -q; then
+if cat /etc/issue | grep -E "(Arch|Manjaro)" -q; then
     bash ${linux_scripts}/install-arch-packages.sh
 else
     bash ${linux_scripts}/install-debs-base.sh
