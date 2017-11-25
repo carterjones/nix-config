f94deb6dbc5eaced57fbdb136a4ee0b508bf65eb
Carter Jones <carter@carterjones.info>
2017-11-25 13:20:08 -0800

    more manjaro stuff

--- scripts/linux/install-vagrant.sh
+++ scripts/linux/install-vagrant.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 pushd /tmp
 
 # Don't run this on Arch, since it automatically handles this via pacman.
-if ! (cat /etc/issue | grep "Arch" -q); then
+if ! (cat /etc/issue | grep -E "(Arch|Manjaro)" -q); then
     wget -nc "https://releases.hashicorp.com/vagrant/1.9.1/vagrant_1.9.1_x86_64.deb" -O vagrant.deb || true
     sudo dpkg -i vagrant.deb
 fi
