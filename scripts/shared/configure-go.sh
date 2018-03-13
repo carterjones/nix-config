f8804abbcd49da21ed9a111126dfcabe40d42cca
Carter Jones <carter@carterjones.info>
2018-03-13 14:21:21 -0700

    update go tools installation

--- scripts/shared/configure-go.sh
+++ scripts/shared/configure-go.sh
@@ -8,14 +8,11 @@ set +u
 # Check for a flag that can be set to avoid configuring go imports.
 if [[ "$SKIP_GO_CONFIGURATION" != "true" ]]; then
 
-    # Install govendor.
-    go get -u github.com/kardianos/govendor
+    # Install dep.
+    go get -u github.com/golang/dep/cmd/dep
 
-    # Install stuff for emacs golang integration.
-    go get -u github.com/golang/lint/golint
-    go get -u github.com/nsf/gocode
-    go get -u golang.org/x/tools/cmd/goimports
-    go get -u github.com/rogpeppe/godef
+    # Install awsinfo utilities.
+    go get -u github.com/carterjones/awsinfo/...
 
 fi
 
