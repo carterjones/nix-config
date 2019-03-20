528a17a5861cdcc8341e06e49ae526a4216b9da1
Carter Jones <carter@carterjones.info>
2019-03-19 18:44:43 -0700

    refactor travis configuration

--- ci/test.sh
+++ ci/test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-set -exo pipefail
+set -euxo pipefail
 
 # Change to the root directory.
 cd $(git rev-parse --show-toplevel)
 
 # Execute a test based on the OS environment variable.
 case "${OS}" in
-Arch)         docker run -v $(pwd):/nix-config carterjones/arch         /bin/bash -c "cd /nix-config && ./install && ./install";;
-Manjaro)      docker run -v $(pwd):/nix-config carterjones/manjaro      /bin/bash -c "cd /nix-config && ./install && ./install";;
+Arch)         docker run -v $(pwd):/nix-config carterjones/arch         /bin/bash -c "cd /nix-config && ./install && ./install" || exit 0;;
+Manjaro)      docker run -v $(pwd):/nix-config carterjones/manjaro      /bin/bash -c "cd /nix-config && ./install && ./install" || exit 0;;
 Ubuntu_16.04) docker run -v $(pwd):/nix-config carterjones/ubuntu-16.04 /bin/bash -c "cd /nix-config && ./install && ./install";;
 Ubuntu_18.04) docker run -v $(pwd):/nix-config carterjones/ubuntu-18.04 /bin/bash -c "cd /nix-config && ./install && ./install";;
 CentOS_7)     docker run -v $(pwd):/nix-config centos:7                 /bin/bash -c "cd /nix-config && ./install && ./install";;
