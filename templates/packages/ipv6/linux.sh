78f41e22e9e819d11c539efcc9c79fb124a81f5d
Carter Jones <carter@carterjones.info>
2019-03-20 13:47:22 -0700

    fix disable-ipv6 service

--- templates/packages/ipv6/linux.sh
+++ templates/packages/ipv6/linux.sh
@@ -8,7 +8,9 @@ sudo chown root:root /etc/systemd/system/disable-ipv6.service
 
 # Make sure this isn't a docker container.
 if [ -f /proc/1/cgroup ] && ! grep -q docker /proc/1/cgroup; then
-    if ! $(sudo systemctl is-active --quiet disable-ipv6); then
+    sudo systemctl enable disable-ipv6
+
+    if ! sudo systemctl is-active --quiet disable-ipv6; then
         sudo systemctl start disable-ipv6
     fi
 fi
