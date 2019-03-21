f1f935c70c3a7054b03646b656019e0b86245ea7
Carter Jones <carter@carterjones.info>
2019-03-21 09:36:24 -0700

    use simpler git installation for centos 7

--- templates/packages/git/centos.sh
+++ templates/packages/git/centos.sh
@@ -3,19 +3,9 @@ set -euxo pipefail
 
 version="2.21.0"
 if ! git --version | grep -q "${version}"; then
-    mkdir /tmp/git
-    pushd /tmp/git
-
-    # curl-devel is required to support https URLs.
-    sudo yum install -y curl-devel
-
-    curl -Lo git.tgz "https://github.com/git/git/archive/v${version}.tar.gz"
-    tar -xvf git.tgz
-    cd "git-${version}"
-    make configure
-    ./configure
-    make all
-    sudo make install
-
+    # Install git.
+    pushd /tmp
+    curl -Lo git.tgz "https://s3-us-west-2.amazonaws.com/res.carterjones.info/pkg/centos7/git/git-${version}.tar.gz"
+    tar -xf git.tgz -C /
     popd
 fi
