5d6cd0fdaa2eb11dff6aa5fd11669b39db0bafcf
Carter Jones <carter@carterjones.info>
2019-03-21 08:00:28 -0700

    install emacs from pre-built tar.gz file

--- templates/packages/emacs/centos.sh
+++ templates/packages/emacs/centos.sh
@@ -1,28 +1,19 @@
 #!/bin/bash
 set -euxo pipefail
 
-# Install prerequisite software.
-sudo yum install -y \
-    gnutls-devel \
-    libjpeg-devel \
-    libpng-devel \
-    libtiff-devel \
-    libungif-devel \
-    libXaw-devel \
-    texinfo
-
 if ! emacs --version | grep -q 26.1.92; then
-    mkdir /tmp/emacs
-    pushd /tmp/emacs
+    # Install dependencies.
+    sudo yum install -y \
+        libtiff \
+        libpng \
+        giflib \
+        libXpm \
+        libXaw \
+        gnutls
 
-    version="26.1.92"
-    curl -Lo emacs.tgz "https://github.com/emacs-mirror/emacs/archive/emacs-${version}.tar.gz"
-    tar -xvf emacs.tgz
-    cd "emacs-emacs-${version}"
-    make configure
-    ./configure
-    make
-    sudo make install
-
+    # Install emacs.
+    pushd /tmp
+    curl -Lo emacs.tgz https://s3-us-west-2.amazonaws.com/res.carterjones.info/pkg/centos7/emacs/emacs-26.1.92.tar.gz
+    sudo tar -xf emacs.tgz -C /
     popd
 fi
