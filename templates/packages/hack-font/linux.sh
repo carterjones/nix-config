32277766fc1e62985c375d3e4f357ad157f53dbe
Carter Jones <carter@carterjones.info>
2019-03-21 08:31:25 -0700

    add sudo for hack-font

--- templates/packages/hack-font/linux.sh
+++ templates/packages/hack-font/linux.sh
@@ -5,7 +5,7 @@ if ! sudo test -f /usr/share/fonts/TTF/Hack-Regular.ttf; then
     cd /tmp
     curl -Lo hack-font.zip https://github.com/chrissimpkins/Hack/releases/download/v2.020/Hack-v2_020-ttf.zip
     unzip -o hack-font.zip
-    mkdir -p /usr/share/fonts/TTF/
+    sudo mkdir -p /usr/share/fonts/TTF/
     for file in Hack-Bold.ttf Hack-BoldItalic.ttf Hack-Italic.ttf Hack-Regular.ttf; do
         sudo chown root:root "${file}"
         sudo mv "${file}" /usr/share/fonts/TTF/
