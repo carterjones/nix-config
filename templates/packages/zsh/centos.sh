7ab6694d6a15d1dcdafbbf0cc6a8622033798b8d
Carter Jones <carter@carterjones.info>
2019-03-23 07:07:07 -0700

    sudo untar zsh on centos 7

--- templates/packages/zsh/centos.sh
+++ templates/packages/zsh/centos.sh
@@ -6,6 +6,6 @@ if ! zsh --version | grep -q "${version}"; then
     # Install zsh.
     pushd /tmp
     curl -Lo zsh.tgz "https://s3-us-west-2.amazonaws.com/res.carterjones.info/pkg/centos7/zsh/zsh-${version}.tar.gz"
-    tar -xf zsh.tgz -C /
+    sudo tar -xf zsh.tgz -C /
     popd
 fi
