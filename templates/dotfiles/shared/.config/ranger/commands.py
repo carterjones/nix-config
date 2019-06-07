2b76b0fdea66a260cbd27ba4a810f39142a8de07
Carter Jones <carter@carterjones.info>
2019-06-07 11:58:52 -0700

    fix fzf <-> ranger integration

--- templates/dotfiles/shared/.config/ranger/commands.py
+++ templates/dotfiles/shared/.config/ranger/commands.py
@@ -1,16 +1,8 @@
-# Please refer to commands_full.py for all the default commands and a complete
-# documentation.  Do NOT add them all here, or you may end up with defunct
-# commands when upgrading ranger.
-
-# A simple command for demonstration purposes follows.
-# -----------------------------------------------------------------------------
-
 from __future__ import (absolute_import, division, print_function)
 
-# You can import any python module as needed.
 import os
+import subprocess
 
-# You always need to import ranger.api.commands here to get the Command class:
 from ranger.api.commands import Command
 
 
@@ -29,47 +21,10 @@ class fzf_select(Command):
     """
 
     def execute(self):
-        import subprocess
-        if self.quantifier:
-            # match only directories
-            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
-            -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
-        else:
-            # match files and directories
-            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
-            -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+        command = r"find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) \
+            -print 2> /dev/null | grep -v '.git$' | grep -v '.git/' | cut -b3- | fzf +m"
         fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
-        stdout, stderr = fzf.communicate()
-        if fzf.returncode == 0:
-            fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
-            if os.path.isdir(fzf_file):
-                self.fm.cd(fzf_file)
-            else:
-                self.fm.select_file(fzf_file)
-
-
-class fzf_locate(Command):
-    """
-    :fzf_locate
-
-    Find a file using fzf.
-
-    With a prefix argument select only directories.
-
-    See:
-    - https://github.com/junegunn/fzf
-    - https://www.youtube.com/watch?v=C64LKCZFzME
-    - https://github.com/gotbletu/shownotes/blob/master/ranger_file_locate_fzf.md
-    """
-
-    def execute(self):
-        import subprocess
-        if self.quantifier:
-            command = "locate home media | fzf -e -i"
-        else:
-            command = "locate home media | fzf -e -i"
-        fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
-        stdout, stderr = fzf.communicate()
+        stdout = fzf.communicate()
         if fzf.returncode == 0:
             fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
             if os.path.isdir(fzf_file):
