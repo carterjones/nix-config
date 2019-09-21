3492b1f9dfb64165d9d10573cbf1afa61680dbd2
Carter Jones <carter@carterjones.info>
2019-09-21 12:17:27 -0700

    run shellcheck against more files

--- ci/lint.sh
+++ ci/lint.sh
@@ -4,5 +4,12 @@ set -euxo pipefail
 # Change to the root directory.
 cd "$(git rev-parse --show-toplevel)"
 
+# Run shellcheck against top level scripts.
+shellcheck install
+shellcheck update.sh
+
 # Run shellcheck against all scripts that end with ".sh".
 find . -path '*/*.sh' -type f -print0 | xargs -0 shellcheck -x
+
+# Run shellcheck against all binfiles.
+find . -path './templates/binfiles/*' -type f -print0 | xargs -0 shellcheck -x
