42b6cfe772b9f86ca99fac325e1652ce09f028b3
Carter Jones <carter@carterjones.info>
2020-02-11 11:05:58 -0800

    add support for running tests as a non-root user

---
+++ ci/bootstrap-tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eux -o pipefail
+
+# Copy the contents of the repository to the notroot user's home directory, so we can build the go
+# binary and run it without getting permission errors.
+cp -R /nix-config /home/notroot/nix-config
+chown -R notroot:notroot /home/notroot/nix-config
+cd /home/notroot/nix-config
+
+# Run the installation the first time.
+./install
+
+# Run the installation a second time so that we can uncover any strange failures that only occur
+# after a previous installation has been run. We anticipate this to take significantly less time
+# than the first installation.
+./install
--- ci/test.sh
+++ ci/test.sh
@@ -4,12 +4,27 @@ set -euxo pipefail
 # Change to the root directory.
 cd "$(git rev-parse --show-toplevel)"
 
-# Execute a test based on the OS environment variable.
-case "${OS}" in
-Arch)         docker run -v "$(pwd):/nix-config" carterjones/arch         /bin/bash -c "cd /nix-config && ./install && ./install" || exit 0;;
-Manjaro)      docker run -v "$(pwd):/nix-config" carterjones/manjaro      /bin/bash -c "cd /nix-config && ./install && ./install" || exit 0;;
-Ubuntu_16.04) docker run -v "$(pwd):/nix-config" carterjones/ubuntu-16.04 /bin/bash -c "cd /nix-config && ./install && ./install";;
-Ubuntu_18.04) docker run -v "$(pwd):/nix-config" carterjones/ubuntu-18.04 /bin/bash -c "cd /nix-config && ./install && ./install";;
-CentOS_7)     docker run -v "$(pwd):/nix-config" centos:7                 /bin/bash -c "cd /nix-config && ./install && ./install";;
-Mac_OS_X)     ./install;;
-esac
+if [[ "${OS}" == "macos" ]]; then
+    # If this is a MacOS environment, run the install script directly.
+    ./install
+else
+    # If this is not a MacOS environment, build a Docker container to run the tests.
+    docker build \
+        --tag "${OS}-ci" \
+        --file "./ci/Dockerfile" \
+        --build-arg IMAGE="carterjones/${OS}" \
+        .
+
+    # Prepare a trap so that Arch and Manjaro testing always succeeds. We don't care if they break
+    # since they're bleeding edge. We expect them to break. We want to log the test results
+    # nonetheless.
+    if [[ "${OS}" == "arch" ]] || [[ "${OS}" == "manjaro" ]]; then
+        function clean_exit {
+            exit 0
+        }
+        trap clean_exit EXIT
+    fi
+
+    # Run the tests.
+    docker run -v "$(pwd):/nix-config" "${OS}-ci" /bin/bash /nix-config/ci/bootstrap-tests.sh
+fi
