670140faf95bb417031d0285b0f571a049862cf6
Carter Jones <carter@carterjones.info>
2020-02-11 15:58:28 -0800

    ensure a display variable is set prior to using dconf

--- templates/packages/gnome/ubuntu.sh
+++ templates/packages/gnome/ubuntu.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 set -euxo pipefail
 
-if apt list --installed 2>/dev/null | grep -q gnome-desktop; then
-    # Set up terminal settings.
-    PROFILE_PATH=/org/gnome/terminal/legacy/profiles:/
-    PROFILE=$(dconf list $PROFILE_PATH)
-    dconf write "${PROFILE_PATH}${PROFILE}scrollback-unlimited" true
-    # Set up workspaces
-    gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ hsize 3
-    gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ vsize 3
+# Ensure gnome is installed.
+if ! apt list --installed 2>/dev/null | grep -q gnome-desktop; then
+    exit 0
 fi
+
+# Ensure this is being run from a terminal with a DISPLAY variable that is set. I have no plans to
+# support running this remotely.
+if [[ -z "${DISPLAY:-}" ]]; then
+    exit 0
+fi
+
+# Set up terminal settings.
+PROFILE_PATH=/org/gnome/terminal/legacy/profiles:/
+PROFILE=$(dconf list $PROFILE_PATH)
+dconf write "${PROFILE_PATH}${PROFILE}scrollback-unlimited" true
+
+# Set up workspaces
+gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ hsize 3
+gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ vsize 3
