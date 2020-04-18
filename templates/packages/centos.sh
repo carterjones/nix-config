5f529a486ddeb424005c03da202d323e83875412
Carter Jones <carter@carterjones.info>
2020-04-17 21:58:30 -0700

    deprecate centos 7

--- templates/packages/centos.sh
+++ templates/packages/centos.sh
@@ -9,6 +9,8 @@ sudo yum install -y \
 sudo yum install -y \
     bzip2 \
     bzip2-devel \
+    emacs \
+    git \
     findutils \
     libffi-devel \
     libffi-devel \
@@ -17,14 +19,12 @@ sudo yum install -y \
     sqlite \
     sqlite-devel \
     xz \
-    xz-devel
+    xz-devel \
+    zsh
 
 sudo yum groupinstall -y "Development Tools"
 
 # shellcheck source=./templates/packages/common.sh
 source ./common.sh
-install_pkg_for_env emacs centos
-install_pkg_for_env git centos
 install_pkg_for_env packer centos
 install_pkg_for_env vscode centos
-install_pkg_for_env zsh centos
