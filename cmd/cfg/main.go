129b2433cf158e2cbd1fe497d0c3af8685711ef3
Carter Jones <carter@carterjones.info>
2020-06-26 15:35:28 -0700

    incorporate shared actions into os actions

--- cmd/cfg/main.go
+++ cmd/cfg/main.go
@@ -3,14 +3,19 @@ package main
 func generateOSActions() []action {
 	osFlag := generateOSFlag()
 
+	sharedActions := []action{
+		func() error { return bash(cnfTemplate("packages/shared.sh")) },
+		func() error { return copy(cnfTemplate("binfiles/shared"), home("bin"), osFlag) },
+	}
+
 	switch {
 	case osFlag.IsMac:
-		return []action{
+		return append([]action{
 			func() error { return bash(cnfTemplate("packages/mac.sh")) },
 			func() error { return copy(cnfTemplate("dotfiles/shared"), home(), osFlag) },
 			func() error { return copy(cnfTemplate("dotfiles/mac"), home(), osFlag) },
 			func() error { return copy(cnfTemplate("binfiles/mac"), home("bin"), osFlag) },
-		}
+		}, sharedActions...)
 	case osFlag.IsArch:
 		fallthrough
 	case osFlag.IsCentos:
@@ -18,28 +23,20 @@ func generateOSActions() []action {
 	case osFlag.IsManjaro:
 		fallthrough
 	case osFlag.IsUbuntu:
-		return []action{
+		return append([]action{
 			func() error { return bash(cnfTemplate("packages/" + osFlag.OSString() + ".sh")) },
 			func() error { return copy(cnfTemplate("dotfiles/linux"), home(), osFlag) },
 			func() error { return copy(cnfTemplate("dotfiles/shared"), home(), osFlag) },
 			func() error { return bash(cnfTemplate("packages/linux.sh")) },
 			func() error { return copy(cnfTemplate("binfiles/linux"), home("bin"), osFlag) },
-		}
+		}, sharedActions...)
 	default:
 		panic("unsupported operating system. bye.")
 	}
 }
 
-func sharedActions() []action {
-	flag := OSFlag{}
-	return []action{
-		func() error { return bash(cnfTemplate("packages/shared.sh")) },
-		func() error { return copy(cnfTemplate("binfiles/shared"), home("bin"), flag) },
-	}
-}
-
 func main() {
-	actions := append(generateOSActions(), sharedActions()...)
+	actions := generateOSActions()
 
 	for _, a := range actions {
 		err := a()
