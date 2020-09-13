468593f7176b9dd556d54323a845489ba065d410
Carter Jones <carter@carterjones.info>
2020-09-13 13:46:38 -0700

    pin to my fork of ramdisk

--- templates/packages/ramdisk/shared.sh
+++ templates/packages/ramdisk/shared.sh
@@ -2,5 +2,13 @@
 set -eux -o pipefail
 
 if ! command -v ramdisk; then
-    GOBIN="${HOME}/bin" go install github.com/mroth/ramdisk/cmd/ramdisk
+    # Due to https://github.com/golang/go/issues/29415, we have to trick go
+    # into thinking that we are in a project in order to pin the version of the
+    # repo we're using here.
+    #
+    # Instead of doing all that, I cloned the repo to avoid installing
+    # potential breaking changes that the original author
+    # (https://github.com/mroth) may make.
+    go get github.com/carterjones/ramdisk/cmd/ramdisk
+    GOBIN="${HOME}/bin" go install github.com/carterjones/ramdisk/cmd/ramdisk
 fi
