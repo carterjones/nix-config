7553ca6b0f20c2293c09c984c6001f387969a357
Carter Jones <carter@carterjones.info>
2021-04-01 21:01:36 -0700

    remove fix specific to ubuntu 16.04

--- templates/packages/openvpn/ubuntu.sh
+++ templates/packages/openvpn/ubuntu.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 set -eux -o pipefail
 
-if grep -q 16.04 /etc/lsb-release; then
-    sudo apt-key add ./repo-public.gpg
-    echo "deb http://build.openvpn.net/debian/openvpn/stable xenial main" > openvpn.list
-    sudo chown root:root openvpn.list
-    sudo mv ./openvpn.list /etc/apt/sources.list.d/
-    sudo apt-get update -y
-fi
-
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y openvpn
