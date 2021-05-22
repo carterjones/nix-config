98df0a25bb4f587e1bed26bd5ff2283cc7c349e8
Carter Jones <carter@carterjones.info>
2021-05-22 09:56:48 -0700

    allow ci to not be set

--- templates/packages/ipv6/linux.sh
+++ templates/packages/ipv6/linux.sh
@@ -5,7 +5,7 @@ sudo cp ./disable-ipv6.service /etc/systemd/system/disable-ipv6.service
 sudo chown root:root /etc/systemd/system/disable-ipv6.service
 
 # Exit the script if this is CI.
-if [[ -n "${CI}" ]]; then
+if [[ -n "${CI:-}" ]]; then
     exit 0
 fi
 
