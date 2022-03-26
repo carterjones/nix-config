783fb1716d83a3284193cdcae826c74b56cd6e3b
Carter Jones <carter@carterjones.info>
2022-03-25 21:34:01 -0700

    install vscode and add code to path

--- templates/packages/vscode/mac.sh
+++ templates/packages/vscode/mac.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 set -eux -o pipefail
 
+if [ ! -d "/Applications/Visual Studio Code.app" ]; then
+    # Install Visual Studio Code from the Microsoft website rather than homebrew
+    # since the update mechanism never seems to work correctly when trying to
+    # update Visual Studio Code if it was installed using homebrew.
+    pushd /tmp
+    curl -Lo code.zip "https://code.visualstudio.com/sha/download?build=stable&os=darwin-universal"
+    unzip -q code.zip
+    mv "Visual Studio Code.app" /Applications/
+    rm code.zip
+    popd
+fi
+
+# Point "code" to Visual Studio Code. This is based on the resulting effect of
+# running the "Shell Command: Install 'code' command in PATH" from within
+# Visual Studio Code, which appears to just add a symlink to /usr/local/bin.
+ln -nfs /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code /usr/local/bin/code
+
 # Configure extensions.
 mkdir -p "${HOME}/Library/Application Support/Code/User/"
 cp -R ./configs/* "${HOME}/Library/Application Support/Code/User/"
