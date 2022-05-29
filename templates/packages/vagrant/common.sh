b20a517d27d887894f7a87af50d5f9901f296a64
Carter Jones <carter@carterjones.info>
2022-05-29 07:11:47 -0700

    fix version selector for vagrant

--- templates/packages/vagrant/common.sh
+++ templates/packages/vagrant/common.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 versions=$(curl --silent "https://releases.hashicorp.com/vagrant/")
-latest_version=$(echo "${versions}" | grep '/vagrant/' | head -1 | sed 's,.*vagrant/,,' | cut -f1 -d '/')
+latest_version=$(echo "${versions}" | grep '/vagrant/' | head -1 | sed 's,.*vagrant/,,;s,".*,,')
 current_version=$(vagrant --version 2>&1 | cut -d" " -f2) || true
 install_plugins="no"
 export latest_version
