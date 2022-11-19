5cdb123230a22ff4fb7ccde0a806aa907b273529
Carter Jones <carter@carterjones.info>
2022-11-19 13:54:32 -0800

    install vagrant using apt on ubuntu

--- templates/packages/vagrant/ubuntu.sh
+++ templates/packages/vagrant/ubuntu.sh
@@ -4,9 +4,8 @@ set -eux -o pipefail
 source ./common.sh
 export install_plugins
 
-if [[ "${latest_version:?}" != "${current_version:?}" ]]; then
-    curl -o /tmp/vagrant_installer "https://releases.hashicorp.com/vagrant/${latest_version}/vagrant_${latest_version}_x86_64.deb"
-    sudo dpkg -i /tmp/vagrant_installer
-fi
+gpg --dearmor < ./ubuntu.gpg | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install -y vagrant
 
 ./plugins.sh
