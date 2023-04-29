33bf0d9c95169dc72cdf9b99696bd362085fa488
Carter Jones <carter@carterjones.info>
2023-04-28 21:41:26 -0700

    simplify hack font installation

--- templates/packages/hack-font/mac.sh
+++ templates/packages/hack-font/mac.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 set -eux -o pipefail
 
-if [ ! -f /Library/Fonts/Hack-Regular.ttf ]; then
-    cd /tmp
-    wget -O hack-font.zip https://github.com/chrissimpkins/Hack/releases/download/v2.020/Hack-v2_020-ttf.zip
-    unzip -o hack-font.zip
-    for file in Hack-Bold.ttf Hack-BoldItalic.ttf Hack-Italic.ttf Hack-Regular.ttf; do
-        sudo chown root "${file}"
-        sudo mv "${file}" /Library/Fonts/
-    done
+if [ ! -f "${HOME}/Library/Fonts/Hack Regular Nerd Font Complete.ttf" ]; then
+    brew tap homebrew/cask-fonts
+    brew install font-hack-nerd-font
 fi
